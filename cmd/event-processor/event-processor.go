f95c8edad98222af5731cc65eb9ff363a0232a0b
periclescesar <periclescesar@gmail.com>
2024-08-30 22:52:37 -0300

    refactor(receiver): move repo instantiation out of message handler

--- cmd/event-processor/event-processor.go
+++ cmd/event-processor/event-processor.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"github.com/periclescesar/event-processor/configs"
 	"github.com/periclescesar/event-processor/internal/receiver"
+	"github.com/periclescesar/event-processor/pkg/mongodb"
 	"github.com/periclescesar/event-processor/pkg/rabbitmq"
 	"log"
 )
@@ -14,7 +16,13 @@ func main() {
 		log.Fatalf("connection failure: %v", err)
 	}
 
-	errCons := rabbitmq.StartConsuming(receiver.EventConsumer)
+	err := mongodb.Connect(context.TODO(), configs.Mongodb.Uri, "event-processor")
+	if err != nil {
+		log.Fatalf("rabbitmq graceful shutdown: %v", err)
+	}
+	eventConsumer := receiver.NewEventConsumer(mongodb.Manager.Db)
+
+	errCons := rabbitmq.StartConsuming(eventConsumer.Handle)
 
 	if errCons != nil {
 		log.Fatalf("consuming failure: %v", errCons)
--- internal/receiver/receiver.go
+++ internal/receiver/receiver.go
@@ -4,30 +4,38 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/periclescesar/event-processor/configs"
 	"github.com/periclescesar/event-processor/internal/event"
 	"github.com/periclescesar/event-processor/internal/repository"
-	"github.com/periclescesar/event-processor/pkg/mongodb"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 )
 
-func EventConsumer(d amqp.Delivery) error {
+type EventConsumer struct {
+	repo *repository.MongoEventRepository
+}
+
+func NewEventConsumer(mongoDb *mongo.Database) *EventConsumer {
+	repo := repository.NewMongoEventRepository(mongoDb)
+
+	return &EventConsumer{repo: repo}
+}
+
+func (ec *EventConsumer) Handle(d amqp.Delivery) error {
 	log.Printf("Received a message: %s", d.Body)
 
 	ev := &event.Event{}
+	// event decode
 	err := json.Unmarshal(d.Body, ev)
 	if err != nil {
 		return fmt.Errorf("json decode: %w", err)
 	}
 
+	// event validate
 	log.Printf("event type: %s", ev.EventType)
-	// event decode
-	// event saveUseCase
-	err = mongodb.Connect(context.TODO(), configs.Mongodb.Uri, "event-processor")
-	repo := repository.NewMongoEventRepository(mongodb.Manager.Db)
 
-	errSave := repo.Save(context.TODO(), ev)
+	// event save
+	errSave := ec.repo.Save(context.TODO(), ev)
 	if errSave != nil {
 		return errSave
 	}
