db81ac1a0c58a29e43e58f6bc9253b99049b4706
periclescesar <periclescesar@gmail.com>
2024-08-31 23:41:37 -0300

    fix(schema): create schema validator

--- pkg/schema-validator/schema-validator.go
+++ pkg/schema-validator/schema-validator.go
@@ -5,44 +5,62 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/qri-io/jsonschema"
+	"os"
+	"path/filepath"
 )
 
 type SchemaValidator struct {
-	rs *jsonschema.Schema
+	registry *jsonschema.SchemaRegistry
 }
 
 func NewSchemaValidator() *SchemaValidator {
-	return &SchemaValidator{}
+	return &SchemaValidator{registry: jsonschema.GetSchemaRegistry()}
 }
 
-func (sv *SchemaValidator) ReadSchema() error {
-	var schemaData = []byte(`{
-  "$id": "file://configs/events-schemas/event-base.schema.json",
-  "$schema": "https://json-schema.org/draft/2019-09/schema",
-  "title": "base for all events",
-  "description": "this schema should be used as a base for all events",
-  "required": ["eventType"],
-  "type": "object",
-  "properties": {
-    "eventType": {
-      "type": "string",
-      "minLength": 3,
-      "maxLength": 255
-    }
-  }
-}`)
-
-	rs := &jsonschema.Schema{}
-	if err := json.Unmarshal(schemaData, rs); err != nil {
-		return fmt.Errorf("unmarshal schema: %w", err)
+func (sv *SchemaValidator) loadSchemaFile(filePath string) (*jsonschema.Schema, error) {
+	byteFile, errR := os.ReadFile(filePath)
+	if errR != nil {
+		return nil, fmt.Errorf("read file %s: %w", filePath, errR)
 	}
 
-	sv.rs = rs
+	rs := &jsonschema.Schema{}
+	if errU := json.Unmarshal(byteFile, rs); errU != nil {
+		return nil, fmt.Errorf("unmarshal schema: %w", errU)
+	}
+
+	return rs, nil
+}
+
+func (sv *SchemaValidator) RegistrySchemasFromPath(path string) error {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf("retriving files on %s: %w", path, err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		fullPath := filepath.Join(path, file.Name())
+		schemaFile, errLoad := sv.loadSchemaFile(fullPath)
+		if errLoad != nil {
+			return errLoad
+		}
+
+		schemaFile.Register("", sv.registry)
+	}
+
 	return nil
 }
 
-func (sv *SchemaValidator) Validate(ctx context.Context, json []byte) error {
-	errs, err := sv.rs.ValidateBytes(ctx, json)
+func (sv *SchemaValidator) Validate(ctx context.Context, uri string, json []byte) error {
+	schema := sv.registry.Get(ctx, uri)
+	if schema == nil {
+		return fmt.Errorf("schema %s not found", uri)
+	}
+
+	errs, err := schema.ValidateBytes(ctx, json)
 	if err != nil {
 		return fmt.Errorf("validate: %w", err)
 	}
