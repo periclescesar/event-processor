cde8faad78c47fc40c379e868ecb5765c5995b4a
periclescesar <periclescesar@gmail.com>
2024-08-31 23:48:44 -0300

    refactor(cmd): instantiate schema validator and repos before handling message

--- cmd/event-processor/event-processor.go
+++ cmd/event-processor/event-processor.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"github.com/periclescesar/event-processor/configs"
 	"github.com/periclescesar/event-processor/internal/receiver"
+	"github.com/periclescesar/event-processor/internal/repository"
 	"github.com/periclescesar/event-processor/pkg/mongodb"
 	"github.com/periclescesar/event-processor/pkg/rabbitmq"
+	schemaValidator "github.com/periclescesar/event-processor/pkg/schema-validator"
 	"log"
 )
 
@@ -20,7 +22,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("rabbitmq graceful shutdown: %v", err)
 	}
-	eventConsumer := receiver.NewEventConsumer(mongodb.Manager.Db)
+
+	sv := schemaValidator.NewSchemaValidator()
+	if sv.RegistrySchemasFromPath("configs/events-schemas") != nil {
+		log.Fatalf("schema validation failure: %v", err)
+	}
+
+	repo := repository.NewMongoEventRepository(mongodb.Manager.Db)
+	eventConsumer := receiver.NewEventConsumer(repo, sv)
 
 	errCons := rabbitmq.StartConsuming(eventConsumer.Handle)
 
