726fb1f0d2c0f327ce2b058e5f8ff36cd7e19c58
periclescesar <periclescesar@gmail.com>
2024-09-01 22:12:12 -0300

    refactor: change to send a event every 5 seconds

--- cmd/event-producer/event-producer.go
+++ cmd/event-producer/event-producer.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
 	"github.com/periclescesar/event-processor/configs"
 	"github.com/periclescesar/event-processor/pkg/rabbitmq"
 	"log"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 func main() {
@@ -14,18 +17,58 @@ func main() {
 		log.Fatalf("connection failure: %v", err)
 	}
 
-	file, errR := os.ReadFile("test/mocked-events/user-created.json")
+	single := flag.Bool("single-message", false, "a bool")
+	eventPath := flag.String("event-path", "test/mocked-events/user-created.json", "path to event, json file")
+
+	if *single {
+		publishSingleMessage(*eventPath)
+		return
+	}
+
+	eventsPath := flag.String("all-event-on-path", "test/mocked-events", "path to all events")
+	loadEventsFromPath(*eventsPath)
+}
+
+func loadEventsFromPath(path string) {
+	for {
+		files, err := os.ReadDir(path)
+		if err != nil {
+			log.Fatalf("retriving files on %s: %v", path, err)
+		}
+
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+
+			fullPath := filepath.Join(path, file.Name())
+			byteFile, errR := os.ReadFile(fullPath)
+			if errR != nil {
+				log.Fatalf("read file %s: %v", fullPath, errR)
+			}
+
+			publish(string(byteFile))
+			time.Sleep(5 * time.Second)
+		}
+	}
+}
+
+func publishSingleMessage(path string) {
+	file, errR := os.ReadFile(path)
 	if errR != nil {
 		log.Fatalf("read event file: %v", errR)
 	}
-	body := string(file)
 
-	err := rabbitmq.Publish("events.exchange", body)
+	publish(string(file))
+}
+
+func publish(message string) {
+	err := rabbitmq.Publish("events.exchange", message)
 	if err != nil {
 		log.Fatalf("publish failure: %v", err)
 	}
 
-	log.Printf("message sent: %v", body)
+	log.Printf("message sent: %v", message)
 
 	errC := rabbitmq.Close()
 	if errC != nil {
