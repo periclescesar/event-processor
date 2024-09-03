12e137c79dac82324fdb9f04a109af3f02663465
periclescesar <periclescesar@gmail.com>
2024-09-03 19:12:37 -0300

    refactor: create db name env

--- cmd/event/processor/processor.go
+++ cmd/event/processor/processor.go
@@ -24,7 +24,7 @@ func main() {
 		log.Fatalf("connection failure: %v", err)
 	}
 
-	err := mongodb.Connect(context.TODO(), configs.Mongodb.URI, "processor")
+	err := mongodb.Connect(context.TODO(), configs.Mongodb.URI, configs.Mongodb.DBname)
 	if err != nil {
 		log.Fatalf("rabbitmq graceful shutdown: %v", err)
 	}
--- configs/mongodb.go
+++ configs/mongodb.go
@@ -3,12 +3,14 @@ package configs
 import "github.com/spf13/viper"
 
 type mongodb struct {
-	URI string
+	URI    string
+	DBname string
 }
 
 var Mongodb *mongodb
 
 func buildMongodbConfig() {
 	var uri = viper.Get("MONGODB_CONNECTION_URI").(string)
-	Mongodb = &mongodb{URI: uri}
+	var dbName = viper.Get("MONGODB_DB_NAME").(string)
+	Mongodb = &mongodb{URI: uri, DBname: dbName}
 }
